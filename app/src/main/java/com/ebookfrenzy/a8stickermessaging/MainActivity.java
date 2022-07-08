a15e099cd3360280e145e46e1a07b48a28e0c447
daisychuying <58837611+daisychuying@users.noreply.github.com>
2022-07-08 16:39:51 -0700

    Edited MainActivity class with Firebase Database

--- app/src/main/java/com/ebookfrenzy/a8stickermessaging/MainActivity.java
+++ app/src/main/java/com/ebookfrenzy/a8stickermessaging/MainActivity.java
@@ -1,5 +1,7 @@
 package com.ebookfrenzy.a8stickermessaging;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +10,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -25,8 +30,38 @@ public class MainActivity extends AppCompatActivity {
         username = findViewById(R.id.editTextUserName);
         btn_signIn = findViewById(R.id.buttonSignIn);
 
-        //Connect with Firebase
+        // Connect with Firebase
         mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        // Update the user info in realtime DB
+        mDatabase.child("users").addChildEventListener(
+                new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                    }
+
+                    @Override
+                    public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                }
+        );
 
         // Redirect to Dashboard Activity after button click
         btn_signIn.setOnClickListener(new View.OnClickListener() {
