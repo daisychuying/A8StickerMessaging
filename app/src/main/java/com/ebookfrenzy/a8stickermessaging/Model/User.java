dd4f4d673d3d9d2e8aa390c0d2829abb2c7c63d8
daisychuying <58837611+daisychuying@users.noreply.github.com>
2022-07-08 15:31:59 -0700

    Added stickers

--- app/src/main/java/com/ebookfrenzy/a8stickermessaging/Model/User.java
+++ app/src/main/java/com/ebookfrenzy/a8stickermessaging/Model/User.java
@@ -1,7 +1,12 @@
 package com.ebookfrenzy.a8stickermessaging.Model;
 
+import java.util.Map;
+
 public class User {
 
     public String username;
-    // public String
+    public Map<Integer, Integer> stickerCount;
+
+    public User(){}
+
 }
