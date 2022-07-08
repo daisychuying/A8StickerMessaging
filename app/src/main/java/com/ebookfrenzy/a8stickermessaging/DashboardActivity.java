30a387bc3e369c9c6ff1625878e571ac59262e0e
Jiayi Nie <nie.jiay@northeastern.edu>
2022-07-08 16:03:15 -0700

    Dashboard page update with Stickers and History fragment

--- app/src/main/java/com/ebookfrenzy/a8stickermessaging/DashboardActivity.java
+++ app/src/main/java/com/ebookfrenzy/a8stickermessaging/DashboardActivity.java
@@ -1,10 +1,23 @@
 package com.ebookfrenzy.a8stickermessaging;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
 
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.ebookfrenzy.a8stickermessaging.Fragments.HistoryFragment;
+import com.ebookfrenzy.a8stickermessaging.Fragments.StickerFragment;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -21,6 +34,54 @@ public class DashboardActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("");
 
         username = findViewById(R.id.username);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        ViewPager viewPager = findViewById(R.id.view_pager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        viewPagerAdapter.addFragment(new StickerFragment(), "Stickers");
+        viewPagerAdapter.addFragment(new HistoryFragment(), "History");
+
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private ArrayList<Fragment> fragments;
+        private ArrayList<String> titles;
+
+        ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+            this.fragments = new ArrayList<>();
+            this.titles = new ArrayList<>();
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            fragments.add(fragment);
+            titles.add(title);
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles.get(position);
+        }
 
     }
 }
--- app/src/main/java/com/ebookfrenzy/a8stickermessaging/MainActivity.java
+++ app/src/main/java/com/ebookfrenzy/a8stickermessaging/MainActivity.java
@@ -1,22 +1,33 @@
 package com.ebookfrenzy.a8stickermessaging;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText username;
-    Button btn_login;
+    Button btn_signIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        username = findViewById(R.id.username);
-        btn_login = findViewById(R.id.btn_login);
+        username = findViewById(R.id.editTextUserName);
+        btn_signIn = findViewById(R.id.buttonSignIn);
+
+        btn_signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, DashboardActivity.class));
+            }
+        });
+
 
     }
 }
