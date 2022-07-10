c4de15225337ce2595d4b74d7140c1600be12053
daisychuying <58837611+daisychuying@users.noreply.github.com>
2022-07-09 22:46:30 -0700

    Updated ReceiveNotification and SendNotification activity and xml

--- app/src/main/java/com/ebookfrenzy/a8stickermessaging/ReceiveNotificationActivity.java
+++ app/src/main/java/com/ebookfrenzy/a8stickermessaging/ReceiveNotificationActivity.java
@@ -3,12 +3,21 @@ package com.ebookfrenzy.a8stickermessaging;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ReceiveNotificationActivity extends AppCompatActivity {
+
+    TextView tv_name_notification;
+    TextView tv_time_notification;
+    ImageView iv_sticker_notification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_notification);
+        tv_name_notification = findViewById(R.id.tv_name_notif);
+        tv_time_notification = findViewById(R.id.tv_time_notif);
+        iv_sticker_notification = findViewById(R.id.iv_sticker_notification);
     }
 }
