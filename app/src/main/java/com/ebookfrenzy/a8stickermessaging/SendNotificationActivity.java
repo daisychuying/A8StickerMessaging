eac374eca187659babc184a3fca4bb3ddd1cabca
daisychuying <58837611+daisychuying@users.noreply.github.com>
2022-07-09 21:45:36 -0700

    Added ReceiveNotification and SendNotification activity

---
+++ app/src/main/java/com/ebookfrenzy/a8stickermessaging/SendNotificationActivity.java
@@ -0,0 +1,13 @@
+package com.ebookfrenzy.a8stickermessaging;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SendNotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_notification);
+    }
+}
